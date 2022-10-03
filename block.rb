30673897f490294eb077948f8eeba7678e0afe97
AKSHAY KAKADE <akshay.kakade.dev@gmail.com>
2022-10-03 17:24:33 +0530

    sample code for trying out ruby blocks

---
+++ block.rb
@@ -0,0 +1,15 @@
+def like_map(array)
+  result = []
+  array.each do |element|
+    result << (yield element)
+  end
+  result
+end
+
+arr = [1,2,7]
+
+result = like_map(arr) do |number|
+  number * 2
+end
+
+p result
